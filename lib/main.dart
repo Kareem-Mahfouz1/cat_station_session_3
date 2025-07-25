459419a08b8fa4ec0d521aea4fdf01c49794a7db
Kareem Mahfouz <kareemmahfouz01@gmail.com>
2025-07-26 02:48:43 +0300

    Refactor MyHomePage layout and styling for improved readability and consistency

--- lib/main.dart
+++ lib/main.dart
@@ -22,93 +22,91 @@ class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromARGB(255, 0, 150, 125),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        spacing: 10,
-        children: [
-          SizedBox(width: double.infinity),
-          CircleAvatar(
-            radius: 50,
-            backgroundImage: AssetImage('assets/images/profile.jpg'),
-          ),
-          Text(
-            "Cat_Reloaded",
-            style: TextStyle(
-              fontSize: 28,
-              fontFamily: "PlaywriteAUQLD",
-              color: Colors.white,
-              fontWeight: FontWeight.w900,
+      // colors:
+      //        Colors.white/red/black/orange
+      //        Color(0XFFF01f30),
+      //        Color.fromARGB(255, 10, 0, 80)
+      backgroundColor: Colors.teal,
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          spacing: 10,
+          children: [
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage('assets/images/profile.jpg'),
             ),
-            textAlign: TextAlign.center,
-          ),
-          Text(
-            'FLUTTER DEVELOPER',
-            style: TextStyle(
-              color: Colors.white70,
-              fontSize: 18,
-              fontWeight: FontWeight.w600,
-              letterSpacing: 2,
+            Text(
+              "Cat_Reloaded",
+              style: TextStyle(
+                fontSize: 28,
+                fontFamily: "PlaywriteAUQLD",
+                color: Colors.white,
+                fontWeight: FontWeight.w900,
+              ),
+              textAlign: TextAlign.center,
             ),
-          ),
-          Divider(color: Colors.grey, thickness: 2, indent: 70, endIndent: 70),
-          Container(
-            // color: Colors.white,  only if no decoration
-            // margin: EdgeInsets.all(50),
-            margin: EdgeInsets.symmetric(horizontal: 40),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5),
+            Text(
+              'FLUTTER DEVELOPER',
+              style: TextStyle(
+                color: Colors.white70,
+                fontSize: 18,
+                fontWeight: FontWeight.w600,
+                letterSpacing: 2,
+              ),
             ),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.phone,
-                  size: 28,
-                  color: const Color.fromARGB(255, 0, 150, 125),
-                ),
-                SizedBox(width: 30),
-                Text(
-                  '010-1232-4567',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 0, 150, 125),
-                    fontSize: 20,
+            Divider(
+              color: Colors.grey,
+              thickness: 2,
+              indent: 70,
+              endIndent: 70,
+            ),
+            Container(
+              // color: Colors.white,  only if no decoration
+              // margin: EdgeInsets.all(50),
+              // margin: EdgeInsets.only(left: 40, right: 40, top: 20),
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.phone, size: 28, color: Colors.teal),
+                  SizedBox(width: 30),
+                  Text(
+                    '010-1232-4567',
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          SizedBox(height: 10),
-          Container(
-            // color: Colors.white,  only if no decoration
-            // margin: EdgeInsets.all(50),
-            margin: EdgeInsets.symmetric(horizontal: 40),
-            padding: EdgeInsets.all(20),
-            decoration: BoxDecoration(
-              color: Colors.white,
-              borderRadius: BorderRadius.circular(5),
-            ),
-            child: Row(
-              children: [
-                Icon(
-                  Icons.mail,
-                  size: 28,
-                  color: const Color.fromARGB(255, 0, 150, 125),
-                ),
-                SizedBox(width: 30),
-                Text(
-                  'someone@example.com',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 0, 150, 125),
-                    fontSize: 20,
+            SizedBox(height: 10),
+            Container(
+              // color: Colors.white,  only if no decoration
+              // margin: EdgeInsets.all(50),
+              margin: EdgeInsets.symmetric(horizontal: 40),
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(5),
+              ),
+              child: Row(
+                children: [
+                  Icon(Icons.mail, size: 28, color: Colors.teal),
+                  SizedBox(width: 30),
+                  Text(
+                    'someone@example.com',
+                    style: TextStyle(color: Colors.teal, fontSize: 20),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
